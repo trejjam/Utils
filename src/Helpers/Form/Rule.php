16a18f68786ac3c74fbc87fd12d83d3bcef89789
Jan Trejbal <jan.trejbal@gmail.com>
2016-12-14 14:33:09 +0100

    Fix date-range validation

--- src/Helpers/Form/Rule.php
+++ src/Helpers/Form/Rule.php
@@ -7,20 +7,36 @@ use Nextras;
 
 class Rule
 {
-	const IPV4      = '(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)';
-	const IPV6      = '(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}';
-	const MAC       = '([a-fA-F0-9]{2}[:.-]?){7}[[a-fA-F0-9]{2}';
+	const IPV4 = '(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)';
+	const IPV6 = '(?:[a-fA-F0-9]{1,4}:){7}[a-fA-F0-9]{1,4}';
+	const MAC  = '([a-fA-F0-9]{2}[:.-]?){7}[[a-fA-F0-9]{2}';
 
 	const DateTimeCompare = 'Trejjam\Utils\Helpers\Form\Rule::dateTimeCompare';
 	const DateCompare     = 'Trejjam\Utils\Helpers\Form\Rule::dateCompare';
 
 	static function dateTimeCompare(Nextras\Forms\Controls\DateTimePicker $item, array $arg)
 	{
-		return $arg[0] <= $arg[1];
+		if (count($arg) > 1) {
+			if ($arg[0] === $item) {
+				trigger_error('Use only second argument, for compare against source control', E_USER_DEPRECATED);
+			}
+
+			return $arg[0] <= $arg[1];
+		}
+
+		return $item->getValue() <= $arg[0];
 	}
 
 	static function dateCompare(Nextras\Forms\Controls\DatePicker $item, array $arg)
 	{
-		return $arg[0] <= $arg[1];
+		if (count($arg) > 1) {
+			if ($arg[0] === $item) {
+				trigger_error('Use only second argument, for compare against source control', E_USER_DEPRECATED);
+			}
+
+			return $arg[0] <= $arg[1];
+		}
+
+		return $item->getValue() <= $arg[0];
 	}
 }
